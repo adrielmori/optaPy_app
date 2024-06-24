7005187bf9b4a332d3e3494db23d54b9bf83b4a3
Gabriel Formigoni <bielformigoni@gmail.com>
2024-06-24 19:59:32 -0300

    Conflito 3

--- constraints.py
+++ constraints.py
@@ -9,9 +9,11 @@ def define_constraints(constraint_factory: ConstraintFactory):
     return [
         # Hard constraints
         teacher_interest_constraint(constraint_factory),
+        teacher_3_consecutive_times(constraint_factory),
         # Soft constraints
         teacher_conflict_1_constraint(constraint_factory),
         teacher_conflict_2_constraint(constraint_factory),
+        teacher_conflict_3_constraint(constraint_factory),
     ]
 
 
@@ -26,6 +28,17 @@ def teacher_interest_constraint(constraint_factory):
         .penalize("Professor não interessado na disciplina", HardSoftScore.ONE_HARD)
     )
 
+def teacher_3_consecutive_times(constraint_factory):
+    # Penaliza professor que ministrou a disciplina nas últimas 3 vezes que foi ofertada
+    return (
+        constraint_factory.for_each(Lesson)
+        .filter(lambda lesson: did_teach_last_3_times(lesson))
+        .penalize(
+            "Ministrou a disciplina nas últimas 3 vezes que foi ofertada",
+            HardSoftScore.ONE_HARD,
+        )
+    )
+
 
 def teacher_conflict_1_constraint(constraint_factory):
     # Recompensa o professor que ministrou a disciplina na última vez que foi ofertada e não a ministrou na penúltima oferta.
@@ -34,7 +47,7 @@ def teacher_conflict_1_constraint(constraint_factory):
         .filter(lambda lesson: did_teach_last_time_but_not_penultimate(lesson))
         .reward(
             "Ministrou a disciplina na última vez e não na penúltima",
-            HardSoftScore.ofSoft(60),
+            HardSoftScore.ofSoft(15),
         )
     )
 
@@ -46,8 +59,16 @@ def teacher_conflict_2_constraint(constraint_factory):
         .filter(lambda lesson: did_teach_last_and_penultimate_time(lesson))
         .reward(
             "Ministrou a disciplina na última e na penúltima vez",
-            HardSoftScore.ofSoft(50),
+            HardSoftScore.ofSoft(10),
         )
+    )
+
+def teacher_conflict_3_constraint(constraint_factory):
+    # Recompensa para o professor que nunca ministrou a disciplina anteriormente.
+    return (
+        constraint_factory.for_each(Lesson)
+        .filter(lambda lesson: is_new_teacher_for_subject(lesson))
+        .reward("Professor nunca ministrou a disciplina", HardSoftScore.ofSoft(5))
     )
 
 
@@ -73,3 +94,19 @@ def did_teach_last_and_penultimate_time(lesson):
     return (
         lesson.teacher.id == last_teacher and lesson.teacher.id == penultimate_teacher
     )
+
+def did_teach_last_3_times(lesson):
+    # Verifica se o professor ministrou a disciplina na última e penúltima vezes que foi ofertada
+    subject_history = lesson.subject.historical_teacher_ids
+    if len(subject_history) < 3:
+        return False
+    last_teacher = subject_history[-1]
+    penultimate_teacher = subject_history[-2]
+    antepenultimate_teacher = subject_history[-3]
+    return (
+        lesson.teacher.id == last_teacher and lesson.teacher.id == penultimate_teacher and lesson.teacher.id == antepenultimate_teacher
+    )
+
+def is_new_teacher_for_subject(lesson):
+    # Verifica se o professor nunca ministrou a disciplina anteriormente.
+    return lesson.teacher.id not in lesson.subject.historical_teacher_ids
